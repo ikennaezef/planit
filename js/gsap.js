1550f9371b5a7696b01783a26884087e2b4d9fd4
Ikenna <iykee0333@gmail.com>
2022-04-23 11:37:11 +0100

    Animation

--- js/gsap.js
+++ js/gsap.js
@@ -35,7 +35,7 @@ convenient = gsap.timeline({
 
 convenient.from('#convenient .convenient__image', {
   opacity: 0,
-  duration: 0.8
+  duration: 1
 })
 .from('#convenient .convenient__text', {
   duration: 1,
@@ -60,4 +60,128 @@ flexible.from('.flexible__grid__text ul li', {
 .from('.flexible__grid__image', {
   duration: 0.8,
   opacity: 0
+})
+
+
+whychoose1 = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.whychoose__reason:nth-child(1)',
+    start: 'top 60%'
+  }
+})
+
+whychoose1.from('.whychoose__reason:nth-child(1)', {
+  y: 20,
+  opacity: 0,
+  duration: 1
+})
+
+whychoose2 = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.whychoose__reason:nth-child(2)',
+    start: 'top 60%'
+  }
+})
+
+whychoose2.from('.whychoose__reason:nth-child(2)', {
+  y: 20,
+  opacity: 0,
+  duration: 1
+})
+
+whychoose3 = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.whychoose__reason:nth-child(3)',
+    start: 'top 60%'
+  }
+})
+
+whychoose3.from('.whychoose__reason:nth-child(3)', {
+  y: 20,
+  opacity: 0,
+  duration: 1
+})
+
+whychoose4 = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.whychoose__reason:nth-child(4)',
+    start: 'top 60%'
+  }
+})
+
+whychoose4.from('.whychoose__reason:nth-child(4)', {
+  y: 20,
+  opacity: 0,
+  duration: 1
+})
+
+
+satisfaction = gsap.timeline({
+  scrollTrigger: {
+    trigger: '#satisfaction',
+    start: 'top 60%'
+  }
+})
+
+satisfaction.from('#satisfaction .satisfaction__image', {
+  opacity: 0,
+  duration: 1
+})
+.from('#satisfaction .satisfaction__text', {
+  duration: 1,
+  opacity: 0,
+  x: 40
+})
+
+
+answer1 = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.answers__answer:nth-child(1)',
+    start: 'top 60%'
+  }
+})
+
+answer1.from('.answers__answer:nth-child(1)', {
+  y: 20,
+  opacity: 0,
+  duration: 1
+})
+
+answer2 = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.answers__answer:nth-child(2)',
+    start: 'top 60%'
+  }
+})
+
+answer2.from('.answers__answer:nth-child(2)', {
+  y: 20,
+  opacity: 0,
+  duration: 1
+})
+
+answer3 = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.answers__answer:nth-child(3)',
+    start: 'top 60%'
+  }
+})
+
+answer3.from('.answers__answer:nth-child(3)', {
+  y: 20,
+  opacity: 0,
+  duration: 1
+})
+
+answer4 = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.answers__answer:nth-child(4)',
+    start: 'top 60%'
+  }
+})
+
+answer4.from('.answers__answer:nth-child(4)', {
+  y: 20,
+  opacity: 0,
+  duration: 1
 })
